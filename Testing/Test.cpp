8b5734de49f3c5577a6f6d5d6677fa08c4acb6c3
Flec2507 <pietbeineke@gmail.com>
2022-05-15 19:17:01 +0200

    Stuff

---
+++ Testing/Test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by pietb on 15.05.2022.
+//
+
+#include "Test.h"
+#include "../BigNum/BigNum.h"
+
+int key[4][4] = {0};
+
+int main() {
+
+
+}
---
+++ Testing/Test.h
@@ -0,0 +1,14 @@
+//
+// Created by pietb on 15.05.2022.
+//
+
+#ifndef DISCORDOCODE_TEST_H
+#define DISCORDOCODE_TEST_H
+
+
+class Test {
+
+};
+
+
+#endif //DISCORDOCODE_TEST_H
